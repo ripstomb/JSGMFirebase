7b592b15ca81c610a303971c07eb4e52d3382e48
Juanse GM <juanse.giraldom@gmail.com>
2024-04-24 10:13:38 -0500

    eso

--- Assets/Scripts/Scripts Juego/GameController.cs
+++ Assets/Scripts/Scripts Juego/GameController.cs
@@ -3,30 +3,28 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public static bool gameOver = false; // Variable para indicar si el juego ha terminado
+    public static bool gameOver = false;
     public GameObject gameOnScreen;
-    public GameObject gameOverScreen; // Referencia a la pantalla de Game Over
+    public GameObject gameOverScreen;
     public ProtaController Prota;
     private void Start()
     {
         gameOver = false;
         gameOnScreen.SetActive(true);
-        gameOverScreen.SetActive(false); // Oculta la pantalla de Game Over al iniciar el juego
+        gameOverScreen.SetActive(false); 
     }
 
     private void Update()
     {
-        // Verifica si la vida del jugador es 0 o menos
         if (Prota.vidaActual <= 0 && !gameOver)
         {
-            gameOver = true; // Establece el estado de Game Over
-            GameOver(); // Llama a la función de Game Over
+            gameOver = true;
+            GameOver();
         }
     }
 
     private void GameOver()
     {
-        // Guarda la puntuación
         GuardarPuntuacion();
         gameOnScreen.SetActive(false);
         gameOverScreen.SetActive(true);
@@ -34,7 +32,6 @@ public class GameController : MonoBehaviour
 
     private void GuardarPuntuacion()
     {
-        // Implementa la lógica para guardar la puntuación aquí
         GameOverController.instance.SaveScore();
     }
 }
